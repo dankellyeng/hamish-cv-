a8237b18f3517271c57ff027bb324d34c4796db5
dankellyeng <danielkelly27@gmail.com>
2025-06-28 14:45:08 +1000

    added image

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import Avatar from '@mui/material/Avatar'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <Container
       sx={{
-        minHeight: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
       <Box textAlign="center">
         <Avatar
-          src="/profile.jpg"
+          src="/assets/profile.jpg"
           alt="Hamish"
-          sx={{ width: 200, height: 200, margin: '0 auto', mb: 2 }}
+          sx={{ width: 200, height: 200, margin: "0 auto", mb: 2 }}
         />
         <Typography variant="h4" component="h1">
           hi, I'm Hamish, please hire me
         </Typography>
       </Box>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
