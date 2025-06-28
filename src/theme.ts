3a523bc0d25119badb1a2dd1904db5badc81a554
Dan Kelly <43508298+dankellyeng@users.noreply.github.com>
2025-06-28 14:36:02 +1000

    Simplify site with Material UI

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: '#ffffff',
+    },
+    text: {
+      primary: '#000000',
+    },
+  },
+})
+
+export default theme
